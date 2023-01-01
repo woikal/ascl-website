4f5ce5bfb2a356704b1f9788eca42b2eb486c44f
Alex Woike-Möller <alexander.woike@gmail.com>
2023-01-01 16:55:50 +0100

    Add self hosted google fonts

---
+++ config/google-fonts.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'fonts' => [
+        'default'          => 'https://fonts.googleapis.com/css2?family=Roboto@1&display=swap',
+        'roboto-condensed' => 'https://fonts.googleapis.com/css2?family=Roboto+Condensed@1&display=swap',
+        'code'             => 'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,400;0,700;1,400&display=swap',
+    ],
+];
